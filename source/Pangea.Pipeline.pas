658ed933a5d00978cb5b7f9f13ce472028d45138
tunc bahcecioglu <tunc.bahcecioglu@gmail.com>
2019-12-26 14:51:38 +0100

    make TPipe a record

--- source/Pangea.Pipeline.pas
+++ source/Pangea.Pipeline.pas
@@ -12,23 +12,18 @@ type
   TMutateFunc<T> = reference to function(const AValue: T): T;
 
 type
-  IPipe<T> = interface['{15112A97-4A0B-4EF5-A123-03830D84A7B2}']
-    function ForEach(const AMutatingFunc: TMutateFunc<T>; const AExecutionPolicy: IExecutionPolicy = nil): IPipe<T>;
-  end;
-
-type
-  TPipe<T> = class(TInterfacedObject, IPipe<T>)
+  TPipe<T> = record
   strict private
     FRange: IRange<T>;
   public
     constructor Create(const ARange: IRange<T>);
-    function ForEach(const AMutatingFunc: TMutateFunc<T>; const AExecutionPolicy: IExecutionPolicy = nil): IPipe<T>;
+    function ForEach(const AMutatingFunc: TMutateFunc<T>; const AExecutionPolicy: IExecutionPolicy = nil): TPipe<T>;
   end;
 
 type
   TPipeline<T> = class
   public
-    class function Start(ARange: IRange<T>): IPipe<T>;
+    class function Start(ARange: IRange<T>): TPipe<T>;
   end;
 
 implementation
@@ -36,17 +31,20 @@ implementation
 uses
   System.SysUtils;
 
-class function TPipeline<T>.Start(ARange: IRange<T>): IPipe<T>;
+class function TPipeline<T>.Start(ARange: IRange<T>): TPipe<T>;
 begin
   Result := TPipe<T>.Create(ARange);
 end;
 
-function TPipe<T>.ForEach(const AMutatingFunc: TMutateFunc<T>; const AExecutionPolicy: IExecutionPolicy): IPipe<T>;
+function TPipe<T>.ForEach(const AMutatingFunc: TMutateFunc<T>; const AExecutionPolicy: IExecutionPolicy): TPipe<T>;
+var
+  LRange: IRange<T>;
 begin
+  LRange := FRange;
   GetExecutionPolicy(AExecutionPolicy).ForAll(FRange.GetInclusiveStart(), FRange.GetInclusiveEnd(),
     procedure(AIndex: Integer)
     begin
-      FRange[AIndex] := AMutatingFunc(FRange[AIndex]);
+      LRange[AIndex] := AMutatingFunc(LRange[AIndex]);
     end);
   Result := Self;
 end;
